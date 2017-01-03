8ee6fd5fdc80f550c6834aca8cc0aa49e07b4a0e
Stan Graafmans <stan_live@hotmail.nl>
2017-01-03 02:46:20 +0100

    Add portal spawning functionality for player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Portal m_PortalPrefab;
+
+    [SerializeField]
+    private LayerMask m_PortalMask = 8;
+
+    private int m_OwnedPortals;
+    public int OwnedPortals { get { return m_OwnedPortals; } }
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_OwnedPortals = 0;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnPortal();
+        }
+    }
+
+    private void SpawnPortal()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f, m_PortalMask))
+        {
+            Portal portal = Instantiate(m_PortalPrefab.gameObject, hit.point + (hit.normal * .01f), Quaternion.identity).GetComponent<Portal>();
+            portal.SetOwner(this);
+            m_OwnedPortals++;
+        }
+    }
+}
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour {
+public class Portal : MonoBehaviour
+{
+    private Player m_Owner;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetOwner( Player player )
+    {
+        m_Owner = player;
+    }
 }
