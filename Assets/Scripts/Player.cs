fd65bcd65feeba4b65f23563fdca6dabf3d18e2e
Stan Graafmans <stan_live@hotmail.nl>
2017-01-03 03:03:31 +0100

    Properly rotate portals to rayhit normal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,13 +14,13 @@ public class Player : MonoBehaviour
     public int OwnedPortals { get { return m_OwnedPortals; } }
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         m_OwnedPortals = 0;
     }
-    
+
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f, m_PortalMask))
         {
-            Portal portal = Instantiate(m_PortalPrefab.gameObject, hit.point + (hit.normal * .01f), Quaternion.identity).GetComponent<Portal>();
+            Portal portal = Instantiate(m_PortalPrefab.gameObject, hit.point + (hit.normal * .01f), Quaternion.Euler(Quaternion.LookRotation(-hit.normal).eulerAngles)).GetComponent<Portal>();
             portal.SetOwner(this);
             m_OwnedPortals++;
         }
