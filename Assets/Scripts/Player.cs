e4e01c3213b9288d5e1c247b6fdc5218b29c8fce
Stan Graafmans <stan_live@hotmail.nl>
2017-01-05 00:48:25 +0100

    Rework the Player prefab & add a Player script
    The player needs to be a parent of the FPSController from the Standard
    Assets that was used until now. Because now a Portal Gun can be added as a
    child to it. Also add a little Player script for the Portal Gun I/O.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private PortalGun m_PortalGun;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_PortalGun != null)
+        {
+            if (Input.GetButtonDown("Fire1")) m_PortalGun.ShootPortalA();
+            if (Input.GetButtonDown("Fire2")) m_PortalGun.ShootPortalB();
+        }
+    }
+}
