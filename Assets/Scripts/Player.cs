4491ecf2f24f79b614070d51f4624a51f6455ba7
Stan Graafmans <stan_live@hotmail.nl>
2017-01-04 22:15:12 +0100

    Fix portal spawning
    Don't spawn portals on top of each other and don't just raycast right
    through illegal colliders, because that made it possible to spawn portals
    behind surfaces that didn't have the LegalSurface layer.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,17 @@ public class Player : MonoBehaviour
     private Portal m_PortalPrefab;
 
     [SerializeField]
-    private LayerMask m_PortalMask = 8;
+    private LayerMask m_LegalSurfaceLayers;
+
+    [SerializeField]
+    private string m_LegalSurfaceTag, m_PortalTag;
 
     private Portal m_PortalA, m_PortalB;
     public Portal PortalA { get { return m_PortalA; } }
     public Portal PortalB { get { return m_PortalB; } }
+
+    [SerializeField]
+    private float m_MaxGunRange = 100f;
 
     // Use this for initialization
     void Start()
@@ -25,39 +31,62 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            if (m_PortalA != null) Destroy(m_PortalA.gameObject);
-            m_PortalA = SpawnPortal();
+            Portal portal = SpawnPortal();
+            if (portal != null)
+            {
+                if (m_PortalA != null) Destroy(m_PortalA.gameObject);
+                m_PortalA = portal;
+            }
         }
 
         if (Input.GetButtonDown("Fire2"))
         {
-            if (m_PortalB != null) Destroy(m_PortalB.gameObject);
-            m_PortalB = SpawnPortal();
+            Portal portal = SpawnPortal();
+            if (portal != null)
+            {
+                if (m_PortalB != null) Destroy(m_PortalB.gameObject);
+                m_PortalB = portal;
+            }
         }
     }
 
     private Portal SpawnPortal()
     {
         RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10f, m_PortalMask))
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+        if (Physics.Raycast(ray, out hit, m_MaxGunRange))
         {
-            GameObject toInstantiate = m_PortalPrefab.gameObject;
-            Vector3 position = hit.point + (hit.normal * .01f);
-
-            Vector3 dir = hit.point - this.transform.position;
-            Vector3 angles = Quaternion.LookRotation(-hit.normal).eulerAngles;
-
-            if (Mathf.Abs(hit.normal.y) == 1f)
+            string hitTag = hit.transform.tag;
+            if (hitTag == m_LegalSurfaceTag || hitTag == m_PortalTag)
             {
-                angles.y = Quaternion.LookRotation(dir).eulerAngles.y;
+                GameObject toInstantiate = m_PortalPrefab.gameObject;
+
+                Vector3 position = hit.point + (hit.normal * .01f);
+
+                Vector3 dir = hit.point - this.transform.position;
+                Vector3 angles = Quaternion.LookRotation(-hit.normal).eulerAngles;
+
+                if (Mathf.Abs(hit.normal.y) == 1f)
+                {
+                    angles.y = Quaternion.LookRotation(dir).eulerAngles.y;
+                }
+
+                Quaternion eulerRot = Quaternion.Euler(angles);
+
+                // If there's any existing portal in the way, destroy it and raycast again to get the right position for the new portal
+                if (hitTag == m_PortalTag)
+                {
+                    Destroy(hit.transform.gameObject);
+                    if (Physics.Raycast(ray, out hit, m_MaxGunRange, m_LegalSurfaceLayers)) position = hit.point + (hit.normal * .01f);
+                }
+
+                Portal portal = Instantiate(toInstantiate, position, eulerRot).GetComponent<Portal>();
+                portal.SetOwner(this);
+
+                return portal;
             }
-
-            Quaternion eulerRot = Quaternion.Euler(angles);
-
-            Portal portal = Instantiate(toInstantiate, position, eulerRot).GetComponent<Portal>();
-            portal.SetOwner(this);
-
-            return portal;
         }
 
         return null;
