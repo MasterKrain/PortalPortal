8cdb674fafdd1b565c8774074f6fee21ee654885
Stan Graafmans <stan_live@hotmail.nl>
2017-01-31 00:03:49 +0100

    Add DynamicLayering script & InsidePortal tag

---
+++ Assets/Scripts/DynamicLayering.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DynamicLayering : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask m_LayerOnEnter, m_LayerOnExit;
+
+    [SerializeField]
+    private string m_UserTag;
+
+    void OnTriggerEnter( Collider other )
+    {
+        if (other.gameObject.tag == m_UserTag)
+        {
+            other.gameObject.layer = m_LayerOnEnter;
+        }
+    }
+
+    void OnTriggerExit( Collider other )
+    {
+        if (other.gameObject.tag == m_UserTag)
+        {
+            other.gameObject.layer = m_LayerOnExit;
+        }
+    }
+}
